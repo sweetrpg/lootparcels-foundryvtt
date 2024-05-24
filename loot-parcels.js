09a99fee0d026fdce3e1ac396e19ec2430e7a03d
Paul Schifferer <paul@schifferers.net>
2024-05-23 21:54:05 -0700

    Cleanup

--- loot-parcels.js
+++ loot-parcels.js
@@ -1,5 +1,4 @@
 import { Config } from "./scripts/config.js";
-// import { Utils } from "./scripts/utilities.js";
 import { handleParcelDrop } from "./scripts/parcels.js";
 import { CypherSystem } from "./scripts/handlers-cyphersystem.js";
 import { A5eSystem } from "./scripts/handlers-a5e.js";
@@ -90,37 +89,6 @@ Hooks.once('setup', async () => {
 			BlackFlagSystem.registerHandlers();
 			break;
 	}
-
-	// Logging.info('Setting up socket...');
-	// game.socket.on(`module.${Config.MODULE.NAME}`, packet => {
-	// 	Logging.debug(packet);
-
-	// 	let data = packet.data;
-	// 	data.receiver = game.actors.get(packet.receiverId);
-	// 	data.trader = game.actors.get(packet.traderId);
-	// 	data.traderUserId = packet.traderUserId;
-	// 	// switch (packet.type) {
-	// 	// 	case 'requestTrade':
-	// 	// 		if (packet.traderUserId != game.user.id &&
-	// 	// 			data.receiver.isOwner &&
-	// 	// 			(!game.user.isGM ||
-	// 	// 				!data.receiver.hasPlayerOwner)) {
-	// 	// 					receiveTrade(data, packet.traderUserId);
-	// 	// 				}
-	// 	// 		break;
-	// 	// 	case 'acceptTrade':
-	// 	// 		if (packet.traderUserId == game.user.id) {
-	// 	// 			endTrade(data)
-	// 	// 		};
-	// 	// 		break;
-	// 	// 	case 'refuseTrade':
-	// 	// 		if (packet.traderUserId == game.user.id) denyTrade(data);
-	// 	// 		break;
-	// 	// 	case 'possessItem':
-	// 	// 		if (packet.traderUserId == game.user.id) alreadyTrade(data);
-	// 	// 		break;
-	// 	// }
-	// });
 });
 
 // Called when dropping something on the character sheet

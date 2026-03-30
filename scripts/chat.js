96c7ef73a036f5b4c9bcced0295019fc998302e2
Paul Schifferer <paul@schifferers.net>
2026-03-29 19:40:15 -0700

    chore: populate first function for chat logging

--- scripts/chat.js
+++ scripts/chat.js
@@ -5,4 +5,20 @@
 import { Logging } from "./logging.js";
 
 export class Chat {
+    /**
+     * Logs a parcel entry to the chat.
+     *
+     * @param {Actor} actor The actor receiving the parcel entry.
+     * @param {String} entryType The type of the parcel entry.
+     * @param {Object} args The arguments associated with the parcel entry.
+     */
+    static async logParcelEntry(actor, entryType, args) {
+        const chatData = {
+            user: game.user.id,
+            speaker: { actor: actor.id, alias: actor.name },
+            content: `<div class="loot-parcel-entry"><strong>${entryType}</strong>: ${JSON.stringify(args)}</div>`,
+        };
+        Logging.debug("Chat.logParcelEntry", chatData);
+        await ChatMessage.create(chatData);
+    }
 };
